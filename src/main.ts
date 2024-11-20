45e14a0411826340563b143c8be2ff2cb6cbf230
FlowCreeper <caiocoutinho2012@hotmail.com>
2024-11-20 18:22:05 -0300

    CORS Resolvido

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://flowcreeper-40741bd8d1ef.herokuapp.com/'); // Allow only desired origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  })
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
